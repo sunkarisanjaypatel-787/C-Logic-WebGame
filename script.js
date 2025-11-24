ede941251f2606a53aae528dd6534a278e8b85a3
sanjaypatel <sunkarisanjaypatel@gmail.com>
2025-11-24 10:53:45 +0530

    Refactor game logic and improve UI interactions

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function initGame(isReset = false) {
     if (isReset) {
         displayMessage(`A new secret number is set! HEY ${playerName.toUpperCase()}! Enter your first guess.`);
     } else {
-        displayMessage(`HEY ${playerName.toUpperCase()}! welcome back TO ~> GUESS THE SECRET NUMBER v2.0. Enter a number from 1-100.`);
+        displayMessage(`HEY ${playerName.toUpperCase()}! welcome TO ~> GUESS THE SECRET NUMBER. Enter a number from 1-100.`);
     }
 
     newGameBtn.classList.add('hidden');
@@ -183,3 +183,4 @@ function handleGuess() {
 
 // --- Start Application on Window Load ---
 window.onload = initializeDom;
+
